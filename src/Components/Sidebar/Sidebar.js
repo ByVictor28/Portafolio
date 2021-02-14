e2c5b9c962bafcbef94e2898a451181800c87fda
victor manuel delfin santos <vdelfin.yunius@gmail.com>
2021-02-13 19:19:42 -0600

    INCIO DEL PROTAFOLIO

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from "./Sidebar.module.css"
+
+const Sidebar = (props)=>{ 
+return (
+    <aside className={classes.Sidebar}>
+        <div>
+            <img src="" alt="Fotografia"></img>
+        </div>
+        <div>
+            
+        </div>
+    </aside>
+) 
+}
+
+export default Sidebar
