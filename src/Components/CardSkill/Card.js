0b785d3d0d5477e1367b5e3c14be68f7c424782c
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2022-01-10 23:24:44 -0600

    tailwind to skills

--- src/Components/CardSkill/Card.js
+++ src/Components/CardSkill/Card.js
@@ -2,12 +2,17 @@ import React, {useContext} from 'react'
 import classes from "./Card.module.css"
 import themeContext from "../../theme";
 
-const Card = ({icon,name})=>{ 
+const Card = ({icon,name,children})=>{ 
     
     const {themeSlected, setThemeSlected} = useContext(themeContext)
     return (
         <div className={`${classes.Card} sombra`} style={{background:themeSlected.background,color:themeSlected.color}}>
-            <i className={icon}></i>
+            {
+                children?
+                children
+                :
+                <i className={icon}></i>
+            }
             <span>{name}</span> 
         </div>
 ) 
