467a0ccdf4ca0c51fcd88d226047c7fb4aa97e9a
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2022-09-03 16:25:53 -0500

    hot fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Portafolio from "./Components/Portafolio/Portafolio";
 import { useContext, useState, useMemo } from "react";
 import themeContext, { themes } from "./theme";
 import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [themeSlected, setThemeSlected] = useState(themes.dark);
@@ -20,28 +21,69 @@ function App() {
   };
 
   return (
-    <themeContext.Provider value={themeProvider}>
-      <div
-        style={{
-          color: themeSlected.color,
-          background: themeSlected.background,
-        }}
-        onScroll={(e) => scrollEvent(e)}
-      >
-        {/* {console.log(themeSlected)} */}
-        <Navbar />
-        <Hero />
-        <div className="container">
-          <About />
-          <Portafolio />
-          <Contact />
+    <>
+      <Helmet>
+        {/*
+    <!-- Primary Meta Tags -->
+    */}
+        <title>Victor Manuel Delfin Santos</title>
+        <meta name="title" content="Victor Manuel Delfin Santos" />
+        <meta
+          name="description"
+          content="🧩 I am a software engineer who is committed to delivering high-quality results. I love working with a team of talented individuals to achieve goals, and to help my fellow team members.
+
+    🧩 Always looking for an opportunity to continue growing professionally and boost my skills in web or mobile development, and if you have any new challenge or opportunity, do not hesitate to contact me."
+        />
+
+        {/*
+    <!-- Open Graph / Facebook -->
+    */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Victor Manuel Delfin Santos" />
+        <meta
+          property="og:description"
+          content="🧩 I am a software engineer who is committed to delivering high-quality results. I love working with a team of talented individuals to achieve goals, and to help my fellow team members.
+
+    🧩 Always looking for an opportunity to continue growing professionally and boost my skills in web or mobile development, and if you have any new challenge or opportunity, do not hesitate to contact me."
+        />
+
+        {/*
+    <!-- Twitter -->
+    */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Victor Manuel Delfin Santos" />
+        <meta
+          property="twitter:description"
+          content="🧩 I am a software engineer who is committed to delivering high-quality results. I love working with a team of talented individuals to achieve goals, and to help my fellow team members.
+
+    🧩 Always looking for an opportunity to continue growing professionally and boost my skills in web or mobile development, and if you have any new challenge or opportunity, do not hesitate to contact me."
+        />
+      </Helmet>
+      <themeContext.Provider value={themeProvider}>
+        <div
+          style={{
+            color: themeSlected.color,
+            background: themeSlected.background,
+          }}
+          onScroll={(e) => scrollEvent(e)}
+        >
+          {/* {console.log(themeSlected)} */}
+          <Navbar />
+          <Hero />
+          <div className="container">
+            <About />
+            <Portafolio />
+            <Contact />
+          </div>
+          <footer>
+            <p>Copyright © 2021. Victor Manuel Delfin Santos</p>
+            <p>{t(`copyright`)}</p>
+          </footer>
         </div>
-        <footer>
-          <p>Copyright © 2021. Victor Manuel Delfin Santos</p>
-          <p>{t(`copyright`)}</p>
-        </footer>
-      </div>
-    </themeContext.Provider>
+      </themeContext.Provider>
+    </>
   );
 }
 
